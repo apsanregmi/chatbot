f4f07a670333f7481f9f09bda39a3e908ac409ca
apsanregmi <mahesh.regmi@cloudproai.com>
2024-10-22 12:31:38 +0545

    deleted

--- local-chatbot/config.js
+++
@@ -1,5 +0,0 @@
-window.env = {
-    WS_URL: "wss://4bwcngjbd2.execute-api.us-east-1.amazonaws.com/Prod",
-    ASSISTANT_ID: "asst_SzVrgEGUGgOsGr5xuszO3oo5",
-    THREAD_ID: "thread_Ljf0eu0BaJFojQaJEHvb9eTt"
-};
